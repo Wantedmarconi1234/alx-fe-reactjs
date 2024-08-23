81b4553a1920bf884e36a3bb9579a6d61da03d86
Wantedmarconi1234 <fredakuffo5@gmail.com>
2024-08-23 17:48:36 +0000

    Added all files

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { AddRecipeForm } from "./AddRecipeForm "
+import { RecipeList } from "./RecipeList "
+
+
+function App() {
+ 
+  return (
+    <>
+      <RecipeList />
+      <AddRecipeForm />
+    </>
+  )
+}
+
+export default App
