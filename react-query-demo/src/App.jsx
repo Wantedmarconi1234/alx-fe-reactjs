2fc5e62e41e1d806168e7208d441221a7a987b80
Wantedmarconi1234 <fredakuffo5@gmail.com>
2024-09-01 01:56:41 +0000

    Added react-query to app.js

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,10 +1,15 @@
 import PostsComponent from "./components/PostsComponent"
+import { QueryClient, QueryClientProvider} from "react-query"
+
+const queryClient = new QueryClient()
 
 function App() {
   
   return (
     <div>
+    <QueryClientProvider client={queryClient}>
       <PostsComponent />
+    </QueryClientProvider>
     </div>
   )
 }
