0d10d7905545d7af6971ae75ebed77f88087799c
Wantedmarconi1234 <fredakuffo5@gmail.com>
2024-09-01 22:54:59 +0000

    Modified test file to .js

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,7 +1,7 @@
 // src/__tests__/TodoList.test.jsx
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import TodoList from '../TodoList';
+import TodoList from '../components/TodoList';
 
 describe('TodoList Component', () => {
   test('renders TodoList component with initial todos', () => {
