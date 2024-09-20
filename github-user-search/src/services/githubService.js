ce138c08140ca084317362ecc2fda4dea3e9ff30
Wantedmarconi1234 <fredakuffo5@gmail.com>
2024-09-20 21:26:57 +0000

    Modified githubService.jsx to githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -3,12 +3,13 @@ import axios from 'axios';
 
 const GITHUB_API_URL = 'https://api.github.com/users';
 
+// Function to fetch GitHub user data
 export const fetchUserData = async (username) => {
   try {
     const response = await axios.get(`${GITHUB_API_URL}/${username}`);
-    return response.data; // Return the user data
+    return response.data; // Return the fetched data
   } catch (error) {
     console.error('Error fetching user data:', error);
-    throw error;
+    throw error; // Throw error if the request fails
   }
 };
