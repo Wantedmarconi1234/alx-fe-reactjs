cd918f4ec6af1cd67dea94c0341c0e6dbde2febe
Wantedmarconi1234 <fredakuffo5@gmail.com>
2024-09-21 17:33:09 +0000

    Added query to API

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -6,15 +6,17 @@ const GITHUB_API_URL = 'https://api.github.com/search/users';
 export const fetchAdvancedUserData = async ({ username, location, minRepoCount }) => {
   let query = '';
 
+  // Build the query string
   if (username) query += `${username} in:login`;
   if (location) query += ` location:${location}`;
   if (minRepoCount) query += ` repos:>${minRepoCount}`;
 
   try {
+    // Make the API call using axios
     const response = await axios.get(`${GITHUB_API_URL}?q=${query}`);
-    return response.data;
+    return response.data;  // Return the user data from the response
   } catch (error) {
     console.error('Error fetching advanced user data:', error);
-    throw error;
+    throw error;  // Throw error so that the component can handle it
   }
 };
